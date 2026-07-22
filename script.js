4a3807668b4f065e926ab22e3a6b78bc22eab5cf
emanikumar54-spec <emanikumar54@gmail.com>
2026-07-22 14:16:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+  font-family:Arial,sans-serif;
+}
 
+body{
+  background:linear-gradient(135deg,#ff9ecb,#ffd6e7);
+  height:100vh;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  overflow:hidden;
+}
+
+.container{
+  text-align:center;
+  color:white;
+}
+
+h1{
+  font-size:48px;
+  margin-bottom:15px;
+}
+
+p{
+  font-size:20px;
+  margin-bottom:25px;
+}
+
+button{
+  padding:15px 35px;
+  border:none;
+  border-radius:50px;
+  background:white;
+  color:#ff4f8b;
+  font-size:20px;
+  cursor:pointer;
+  transition:.3s;
+}
+
+button:hover{
+  transform:scale(1.1);
+    }
